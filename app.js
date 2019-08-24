f5ea66bf560be717a03d16fc52f0ecc6a8c453dd
vignesh <vigneshit13@gmail.com>
2019-08-24 17:29:48 +0530

    Routing files created

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
+const express = require('express');
 const fs = require('fs');
-const notes  = require('./notes');
-const msg = notes();
+const path=require('path');
+const app = express();
 
-fs.writeFileSync('notes.txt', 'File is created.');
-fs.appendFileSync('notes.txt', '  This text is appended in notes.txt. ');
-fs.appendFileSync('notes.txt', msg);
+app.listen(3000, () => console.log('Server running at 3000...'));
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'/public','home.html'))
+})
+
+app.get('/create', (req, res) => {
+    res.sendFile(path.join(__dirname,'/public','create.html'))
+})
+
+app.get('/view', (req, res) => {
+    res.sendFile(path.join(__dirname,'/public','view.html'))
+})
+
+// const msg = notes();
+// fs.writeFileSync('notes.txt', 'File is created.');
+// fs.appendFileSync('notes.txt', '  This text is appended in notes.txt. ');
+// fs.appendFileSync('notes.txt', msg);
