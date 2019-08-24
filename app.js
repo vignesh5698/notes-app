78d7f3d0b5a8e8dcf924d9ddc46c76c6c57207dd
vignesh <vigneshit13@gmail.com>
2019-08-24 18:10:12 +0530

    Adding posts to the file

--- app.js
+++ app.js
@@ -2,20 +2,35 @@ const express = require('express');
 const fs = require('fs');
 const path=require('path');
 const app = express();
+const  bodyParser=require("body-parser");
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 app.listen(3000, () => console.log('Server running at 3000...'));
 app.use(express.static(path.join(__dirname,'public')))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'/public','home.html'))
+    res.sendFile(path.join(__dirname,'/public','home.html'));
 })
 
 app.get('/create', (req, res) => {
-    res.sendFile(path.join(__dirname,'/public','create.html'))
+    res.sendFile(path.join(__dirname,'/public','create.html'));
+})
+
+app.post('/create', (req, res) => {
+    let notesName = req.body.notesName;
+    let notesContent = req.body.notesContent;
+    if (!fs.existsSync(`posts/${notesName}`)) {
+        fs.writeFileSync(`posts/${notesName}`, notesContent);
+        res.send('Notes saved...');
+    } else {
+        res.send('Notes already exist. Try it with another name.')
+    }
+    // res.redirect('/')
 })
 
 app.get('/view', (req, res) => {
-    res.sendFile(path.join(__dirname,'/public','view.html'))
+    res.sendFile(path.join(__dirname,'/public','view.html'));
 })
 
 // const msg = notes();
